aee220cddcb7d8c0f5f1100e2459dcbf2b7556e8
PUSPO <107134550+pacman-cli@users.noreply.github.com>
2026-01-03 22:13:27 +0600

    Time: 1060 ms (9.99%) | Memory: 0B (100.00%) - LeetSync

---
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -0,0 +1,13 @@
+SELECT 
+   d.name AS Department,
+   e.name AS Employee ,
+   e.salary AS Salary
+FROM Employee e
+JOIN Department d
+ON e.departmentId = d.id
+WHERE (e.salary, e.departmentId)
+In(
+    SELECT MAX(salary),departmentId
+    FROM Employee
+    GROUP BY departmentId
+);

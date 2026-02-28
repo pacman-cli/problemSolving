39d7a6d2639d7729eec329c276605722d1f434ae
MD. Ashikur Rahman Puspo <107134550+pacman-cli@users.noreply.github.com>
2026-03-01 02:52:48 +0600

    Time: 221 ms (72.11%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,12 @@
+
+-- GROUP(rating/position + rating/positon)
+-- COUNT(*) * 100 -> in a group
+SELECT 
+    query_name,
+    ROUND(AVG(rating::decimal/position),2) AS quality,
+    ROUND(
+        100.0*SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) /COUNT(*)
+        ,2
+    ) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name
